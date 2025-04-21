dc0afe5b3a4f5dbafc9e6be7fa1254011d8609fe
lnx00 <34384416+lnx00@users.noreply.github.com>
2025-04-22 01:39:59 +0200

    Minor syntax improvement

--- src/patches/disable_integrity_checks.rs
+++ src/patches/disable_integrity_checks.rs
@@ -1,7 +1,11 @@
 use windows::{
     Wdk::System::Threading::{NtQueryInformationThread, ThreadQuerySetWin32StartAddress},
-    Win32::System::Threading::{OpenThread, THREAD_ALL_ACCESS, TerminateThread},
+    Win32::System::{
+        LibraryLoader::GetModuleHandleA,
+        Threading::{OpenThread, THREAD_ALL_ACCESS, TerminateThread},
+    },
 };
+use windows_strings::s;
 
 const INTEGRITY_THREAD_START_ADDRESS: usize = 0x14275DE50;
 
@@ -10,11 +14,8 @@ fn check_thread(thread_id: u32) -> Result<bool, String> {
         let mut thread_start_address = 0x0;
 
         // Get a handle to the thread
-        let thread_handle = OpenThread(THREAD_ALL_ACCESS, false, thread_id);
-        let thread_handle = match thread_handle {
-            Ok(handle) => handle,
-            Err(_) => return Err("failed to open thread handle".to_string()),
-        };
+        let thread_handle = OpenThread(THREAD_ALL_ACCESS, false, thread_id)
+            .map_err(|_| "failed to open thread handle")?;
 
         // Query the thread start address
         let nt_status = NtQueryInformationThread(
@@ -33,10 +34,7 @@ fn check_thread(thread_id: u32) -> Result<bool, String> {
         }
 
         if thread_start_address == INTEGRITY_THREAD_START_ADDRESS {
-            let result = TerminateThread(thread_handle, 0x0);
-            if result.is_err() {
-                return Err("failed to terminate thread".to_string());
-            }
+            TerminateThread(thread_handle, 0x0).map_err(|_| "failed to terminate thread")?;
 
             return Ok(true);
         }
@@ -45,9 +43,7 @@ fn check_thread(thread_id: u32) -> Result<bool, String> {
     Ok(false)
 }
 
-pub fn disable_integrity_checks() -> Result<(), String> {
-    println!("Disabling integrity checks...");
-
+fn terminate_integrity_checks() -> Result<(), String> {
     let process_id = libmem::get_process().unwrap().pid;
     let thread_list = libmem::enum_threads().ok_or("failed to enumerate threads")?;
 
@@ -68,6 +64,14 @@ pub fn disable_integrity_checks() -> Result<(), String> {
             }
         }
     }
+
+    Ok(())
+}
+
+pub fn disable_integrity_checks() -> Result<(), String> {
+    println!("Disabling integrity checks...");
+
+    terminate_integrity_checks()?;
 
     Ok(())
 }

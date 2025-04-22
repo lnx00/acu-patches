e55816f1960e2c407500a65dd5114318eb5540cc
lnx00 <34384416+lnx00@users.noreply.github.com>
2025-04-22 03:08:52 +0200

    Hook CreateThread to prevent integrity check startup

--- src/patches.rs
+++ src/patches.rs
@@ -1,8 +1,10 @@
-pub mod disable_integrity_checks;
 pub mod disable_camera_smoothing;
+pub mod disable_integrity_checks;
 
 pub fn run_all_patches() -> Result<(), String> {
     disable_integrity_checks::disable_integrity_checks()?;
+
+    std::thread::sleep(std::time::Duration::from_secs(20));
     disable_camera_smoothing::disable_camera_smoothing()?;
 
     Ok(())
--- src/patches/disable_integrity_checks.rs
+++ src/patches/disable_integrity_checks.rs
@@ -1,13 +1,32 @@
+use std::ffi::c_void;
+
 use windows::{
     Wdk::System::Threading::{NtQueryInformationThread, ThreadQuerySetWin32StartAddress},
-    Win32::System::{
-        LibraryLoader::GetModuleHandleA,
-        Threading::{OpenThread, THREAD_ALL_ACCESS, TerminateThread},
+    Win32::{
+        Foundation::HANDLE,
+        System::{
+            LibraryLoader::{GetModuleHandleA, GetProcAddress},
+            Threading::{
+                LPTHREAD_START_ROUTINE, OpenThread, THREAD_ALL_ACCESS, THREAD_CREATE_SUSPENDED,
+                THREAD_CREATION_FLAGS, TerminateThread,
+            },
+        },
     },
 };
 use windows_strings::s;
 
 const INTEGRITY_THREAD_START_ADDRESS: usize = 0x14275DE50;
+
+static mut ORIGINAL_CREATE_THREAD: Option<
+    unsafe extern "system" fn(
+        *mut c_void,
+        usize,
+        *mut c_void,
+        *mut c_void,
+        THREAD_CREATION_FLAGS,
+        *mut u32,
+    ) -> HANDLE,
+> = None;
 
 fn check_thread(thread_id: u32) -> Result<bool, String> {
     unsafe {
@@ -68,9 +87,59 @@ fn terminate_integrity_checks() -> Result<(), String> {
     Ok(())
 }
 
+unsafe extern "system" fn empty_thread(_: *mut c_void) -> u32 {
+    return 0;
+}
+
+extern "system" fn hk_create_thread(
+    lp_thread_attributes: *mut c_void,
+    dw_stack_size: usize,
+    mut lp_start_address: *mut c_void,
+    lp_parameter: *mut c_void,
+    dw_creation_flags: THREAD_CREATION_FLAGS,
+    lp_thread_id: *mut u32,
+) -> HANDLE {
+    unsafe {
+        if lp_start_address as usize == INTEGRITY_THREAD_START_ADDRESS {
+            println!("CreateThread: preventing integrity check thread creation");
+            lp_start_address = empty_thread as *mut c_void;
+        }
+
+        return ORIGINAL_CREATE_THREAD.unwrap()(
+            lp_thread_attributes,
+            dw_stack_size,
+            lp_start_address,
+            lp_parameter,
+            dw_creation_flags,
+            lp_thread_id,
+        );
+    }
+}
+
+fn hook_integrity_checks() -> Result<(), String> {
+    unsafe {
+        let kernel32_handle = GetModuleHandleA(s!("kernel32.dll"))
+            .map_err(|_| "failed to get kernel32.dll handle")?;
+
+        let fp_create_thread = GetProcAddress(kernel32_handle, s!("CreateThread"))
+            .ok_or("failed to get CreateThread address")?;
+
+        let create_thread_address = fp_create_thread as *mut c_void as usize;
+        let hook_address = hk_create_thread as *mut c_void as usize;
+
+        let trampoline = libmem::hook_code(create_thread_address, hook_address)
+            .ok_or("failed to hook CreateThread")?;
+
+        ORIGINAL_CREATE_THREAD = trampoline.callable();
+    }
+
+    Ok(())
+}
+
 pub fn disable_integrity_checks() -> Result<(), String> {
     println!("Disabling integrity checks...");
 
+    hook_integrity_checks()?;
     terminate_integrity_checks()?;
 
     Ok(())
